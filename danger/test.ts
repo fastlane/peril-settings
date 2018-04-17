cc1f41ba936ec400db75c080675a9bed5b97cb12
Josh Holtz <josh@rokkincat.com>
2018-04-16 21:38:47 -0500

    Update test.ts

--- danger/test.ts
+++ danger/test.ts
@@ -4,4 +4,11 @@ import { schedule, danger } from "danger";
 import { IncomingWebhook } from "@slack/client";
 import { Issues } from "github-webhook-event-types"
 
+declare const peril: any // danger/danger#351
+const gh = danger.github as any as Issues
+const issue = gh.issue
+const repo = gh.repository
+const text = (issue.title + issue.body).toLowerCase()
+const api = danger.github.api
+
 console.log("end test");

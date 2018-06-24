fa072ff69a1f8251dbdb71b4703bebdb2568b8d8
Kevin Samoei <samoeikev@gmail.com>
2018-06-24 03:48:04 +0300

    enable peril to add labels to PRs

---
+++ danger/pr.ts
@@ -0,0 +1,20 @@
+import {schedule, danger} from "danger";
+
+declare const peril : any
+const gh = danger.github
+const pr = gh.pr
+const repo = gh.repository
+const api = danger.github.api
+const pr_date = pr.created_at
+const now = new Date()
+const weekAgo = now.setDate(now.getDay() - 7)
+
+if (Date.parse(pr_date) < weekAgo) {
+    schedule(async() => {
+        await api.pr.addLabels({
+          owner: repo.owner.login,
+          repo: repo.name,
+          number: pr.number,
+          labels: ["needs attention"]})
+    });
+}

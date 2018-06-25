c553b2fcd7fe4c05ac400bc32f088b4299fbf0be
Kevin Samoei <samoeikev@gmail.com>
2018-06-25 19:38:03 +0300

    Remove unused code

--- danger/pr.ts
+++ danger/pr.ts
@@ -1,7 +1,5 @@
 import {schedule, danger} from "danger";
-import {gh, repo, api} from './regressions'
 
-declare const peril : any
 const gh = danger.github
 const pr = gh.pr
 const repo = gh.repository
@@ -12,8 +10,10 @@ const weekAgo = now.setDate(now.getDay() - 7)
 
 if (Date.parse(pr_date) < weekAgo) {
     schedule(async() => {
-        await api
-            .pr
-            .addLabels({owner: repo.owner.login, repo: repo.name, number: pr.number, labels: ["needs attention"]})
+        await api.pr.addLabels({
+          owner: repo.owner.login,
+          repo: repo.name,
+          number: pr.number,
+          labels: ["needs attention"]})
     });
 }
